dd9bf8b31ff0e959c5d9d20434d331c83505a66b
Spedine <tucogamesytthegamesof@gmail.com>
2024-05-18 00:03:14 -0300

    feat: setup cors config + spring doc config

---
+++ src/main/java/com/hospital_api/infra/doc/SpringDocConfigurations.java
@@ -0,0 +1,31 @@
+package com.hospital_api.infra.doc;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfigurations {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                .addSecuritySchemes("bearer-key",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("Hospital model RESTful API")
+                        .description("RESTful API for a Hospital model, with CRUD of employees and patients, " +
+                                "in addition to scheduling and canceling an appointment")
+                        .contact(new Contact()
+                                .name("Project Author")
+                                .email("spedinearthur@gmail.com")));
+    }
+}
