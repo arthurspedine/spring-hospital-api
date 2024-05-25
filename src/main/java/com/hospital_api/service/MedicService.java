65413eaf9e0ae2a517e779795fec773af377561e
Spedine <tucogamesytthegamesof@gmail.com>
2024-05-25 09:53:46 -0300

    fix: removing import

--- src/main/java/com/hospital_api/service/MedicService.java
+++ src/main/java/com/hospital_api/service/MedicService.java
@@ -12,7 +12,6 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
